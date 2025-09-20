a8e15ad7b9522782be76da73494e7e3fe60f72e6
eduardo-lamounier <a2025952788@teiacoltec.org>
2025-09-19 23:32:27 -0300

    Fazendo a questão 1 da lista 10 de Leandro

--- Introducao-a-programacao/3o-tri/Listas-Leandro/Lista10/codigo.c
+++ Introducao-a-programacao/3o-tri/Listas-Leandro/Lista10/codigo.c
@@ -2,33 +2,62 @@
 
 #define TEXTO_MAX 30
 
-struct Aluno{
+struct DadosAluno{
     int Matricula;
     char *Nome;
     char *Curso;
     float Altura;
 };
+typedef struct DadosAluno Aluno;
 
-// outras funções (estou com preguiça agora)
+void leituraDadosAluno(Aluno *aluno){
 
-struct Aluno novoAluno(){
-    struct Aluno aluno;
-
-    printf("Digite seu nome: ");
-    fgets(aluno.Nome, TEXTO_MAX, stdin);
+    printf("Digite seu nome[max: %d caracteres]: ", TEXTO_MAX);
+    fgets(aluno->Nome, TEXTO_MAX, stdin);
 
     printf("Digite seu curso: ");
-    fgets(aluno.Curso, TEXTO_MAX, stdin);
+    fgets(aluno->Curso, TEXTO_MAX, stdin);
 
-    printf("Digite seu numero de matricula: ");
-    scanf("%d", &aluno.Matricula);
+    printf("Digite seu numero de matricula[max: %d caracteres]: ", TEXTO_MAX);
+    scanf("%d", &aluno->Matricula);
 
     printf("Digite sua altura: ");
-    scanf("%f", &aluno.Altura);
-
-    return aluno;
+    scanf("%f", &aluno->Altura);
 }
 
 int main(int argc, char **argv){
+    int opc = 0;
+    int parar = 0;
 
+    while(!parar){
+        printf("Digite o numero correspondente a questao que deseja rodar:\n");
+        printf("1. Rodar Questao 01;\n");
+        printf("2. Rodar Questao 02;\n");
+        printf("3. Rodar Questao 03;\n");
+        printf("4. Rodar Questao 04;\n");
+        printf("0. Sair.\n");
+
+        scanf("%d", &opc);
+        switch(opc){
+            case 0:
+                parar = 1;
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            default:
+            printf("Valor de entrada inválido, tente novamente");
+        }
+    }
+
+    return 0;
 }
